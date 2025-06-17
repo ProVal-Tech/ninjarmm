5fbe237beff97e5d1ab1a5df028c1905088110f3
Stephen <11358399+s-nix@users.noreply.github.com>
2025-06-17 15:46:44 -0400

    Push scripts template

---
+++ scripts/_template.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    Short summary of what this script does.
+
+.NOTES
+    [script]
+    name = "Template Script"
+    description = "A brief description of the script's purpose"
+    categories = ["Category1", "Category2"]
+    language = "PowerShell"
+    operating_system = "Windows"
+    architecture = "<arch>"
+    run_as = "<context>"
+
+    [[script.variables]]
+    name = "Variable 1"
+    description = "Description of Variable 1"
+    type = "<type>"
+    mandatory = "true|false"
+    default_value = "<default_value>"
+    option_values = ["<option1>", "<option2>"]
+    top_option_is_default = "true|false"
+
+    [[script.variables]]
+    name = "Variable 2"
+    description = "Description of Variable 2"
+    type = "<type>"
+    mandatory = "true|false"
+    default_value = "<default_value>"
+    option_values = ["<option1>", "<option2>"]
+    top_option_is_default = "true|false"
+#>
+
+# Write your PowerShell script here
